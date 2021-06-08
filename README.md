e23606cd0498f6c909baa81dfcad368e3514e705
SiHubb <simonjameshubbard@gmail.com>
2021-06-08 12:50:48 +0100

    Callback functionality building, visually messy

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 dataX
 =====
 
-Web app for engineering data exploration and test suggestion
+Web app for engineering data exploration and test suggestion. Loads in a local data file 
+and presents a dashboard to show relationships and leverage Bayesian optimisation to 
+suggest next test paremeters.
 
 ##Usage
 
--- app.py
+++ app.py
@@ -1,9 +1,31 @@
 import dash
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+from dash.dependencies import Input, Output, State
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import dash_table
+
+fileLoc='data/logs1.csv'
+test_data = pd.read_csv(fileLoc)
+
+df = px.data.iris()
+fig = px.parallel_coordinates(test_data, color="mix",
+                                dimensions=['p1', 'p2', 'p3', 'p4','mix'],
+                                color_continuous_scale=px.colors.diverging.Earth,
+                                color_continuous_midpoint=0.15)
+
 
 external_stylesheets = [dbc.themes.BOOTSTRAP]
-
+# external_stylesheets = [
+#     {
+#         "href": "https://fonts.googleapis.com/css2?"
+#         "family=Poppins:wght@300&display=swap",
+#         "rel": "stylesheet",
+#     },
+# ]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
@@ -18,19 +40,53 @@ app.layout = html.Div([
                     dbc.Col('Upload or create an original dataset, explore relationships and '
                     'get recommended next parameters to test', width=8),
                 ]),
-                dbc.Tabs([
-                    dbc.Tab([
+                dbc.Row([
+                    html.P('You are looking at the data in: '+fileLoc)
+                ]),
+                dbc.Row([
+                    dbc.Col(
+                        dash_table.DataTable(
+                            data=test_data.to_dict('records'),
+                            columns=[{'name': i, 'id': i} for i in test_data.columns]
+                        ),width=8
+                    )
+                ]),
+                dbc.Row([
+                    dbc.Col(dcc.Graph(figure=fig),width=10)
+                ]),
+                dcc.Dropdown(
+                    id='p_dropdown',
+                    value='p1',
+                    options=[{'label': Parameter, 'value': Parameter} for Parameter in test_data.columns]
+                ),
+                dcc.Graph(id='scatter_graph'),
+                dcc.Dropdown(
+                    id='x_bayes',
+                    value='p1',
+                    options=[{'label': Parameter, 'value': Parameter} for Parameter in test_data.columns],
+                    multi=True
+                ),
+                html.P(id='x_out')
 
-                    ],label='Upload/Generate Data'),
-                    dbc.Tab([
 
-                    ],label='Explore Data'),
-                    dbc.Tab([
 
-                    ],label='Recommend Data Exploration')
-                ])
 
             ])
+@app.callback(Output('scatter_graph','figure'),
+              Input('x_bayes','value'))
+def plotOneVar(choose_x):
+    fig = go.Figure()
+    if type(choose_x)==str:
+        fig.add_scatter(x=test_data[choose_x], y=test_data['mix'])
+    elif type(choose_x)==list:
+        for i in choose_x:
+            fig.add_scatter(x=test_data[i], y=test_data['mix'])
+    return fig
+
+@app.callback(Output('x_out','children'),
+              Input('x_bayes','value'))
+def tellMe(choose_x):
+    return choose_x
 
 if __name__ == '__main__':
     app.run_server(debug=True)
